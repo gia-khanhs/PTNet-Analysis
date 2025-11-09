912ea8678d80b14545e84de18626c98fb4fd52d5
--global <kgia3127@gmail.com>
2025-11-09 17:51:50 +0700

    add comments to main.py

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ from topoDataIO import buildAndSave, loadGraph
 graph = loadGraph()
 
 # graph[0] = node list
+# graph[0][i].name/address = Attributes of the node ith
+
 # graph[1] = graph's adjacency list
+# graph[1][u] = a list of edges that connect u and graph[1][u][i]
+# graph[1][u][i].destination/distance/travelTime = self-explainatory
