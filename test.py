492ad187dfba25ee2028ddc8111ce43e4dd49f6b
--global <kgia3127@gmail.com>
2025-12-13 15:07:23 +0700

    add multiProcessing utilities

--- test.py
+++ test.py
@@ -1,26 +1,8 @@
-import multiprocessing as mp
-from threading import Thread
+from utilities.multiProc import multiProcFunc
 
-def runChunk(lr):
-    l, r = lr
+def lol(rng):
+    l, r = rng
     return (l, r)
 
-def test():
-    N = 10
-
-    nProcs = min(N, 4)
-    chunkSize = int(N / nProcs)
-    chunk = []
-    preR = 0
-    for i in range(nProcs):
-        if i == nProcs - 1: chunk.append((preR + 1, N))
-        else: 
-            chunk.append((preR + 1, preR + chunkSize))
-            preR = preR + chunkSize
-
-    
-    if __name__ == "__main__":
-        with mp.Pool(nProcs) as p:
-            print(p.map(runChunk, chunk))
-
-test()
+if __name__ == "__main__":
+    print(multiProcFunc(lol, 10))
---
+++ utilities/multiProc.py
@@ -0,0 +1,25 @@
+import multiprocessing
+
+cpuCount = multiprocessing.cpu_count()
+
+def chunk(N, nProcs = cpuCount):
+    nProcs = min(N, nProcs)
+    chunkSize = int(N / nProcs)
+    chunks = []
+    preR = 0
+    
+    for i in range(nProcs):
+        if i == nProcs - 1: chunks.append((preR + 1, N))
+        else:
+            chunks.append((preR + 1, preR + chunkSize))
+            preR = preR + chunkSize
+
+    return chunks
+
+def multiProcFunc(func, N, nProcs = cpuCount):
+    chunks = chunk(N, nProcs)
+
+    with multiprocessing.Pool(len(chunks)) as p:
+        ret = p.map(func, chunks)
+
+    return ret
